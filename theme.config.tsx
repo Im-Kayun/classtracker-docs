2141afe18abc4953aa9761ecd5877cdd6e817886
Kayun <kayunowna@gmail.com>
2024-01-12 19:15:01 +0000

    Initial commit

---
+++ theme.config.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { DocsThemeConfig } from 'nextra-theme-docs'
+
+const config: DocsThemeConfig = {
+  logo: <span>Class Tracker</span>,
+  docsRepositoryBase: 'https://github.com/shuding/nextra-docs-template',
+  footer: {
+    text: 'Class Tracker',
+  },
+}
+
+export default config
